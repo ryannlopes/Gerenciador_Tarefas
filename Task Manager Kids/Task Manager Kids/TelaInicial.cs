87bdbf6cf1482b09576e37a40519b6ca89e1fc83
Panizo2 <88464143+Panizo2@users.noreply.github.com>
2023-05-25 22:30:20 -0300

    App
    Iniciando aplicativo

---
+++ Task Manager Kids/Task Manager Kids/TelaInicial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Manager_Kids
+{
+    public partial class TelaInicial : Form
+    {
+        public TelaInicial()
+        {
+            InitializeComponent();
+
+            MenuEscolhido = "";
+        }
+
+        private string MenuEscolhido;
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            MenuEscolhido = "Responsavel";
+
+            Login loginscreen = new Login(MenuEscolhido);
+            loginscreen.Show();
+            this.Hide();
+        }
+
+        private void guna2Button5_Click(object sender, EventArgs e)
+        {
+            MenuEscolhido = "Filho";
+
+            Login loginscreen = new Login(MenuEscolhido);
+            loginscreen.Show();
+            this.Hide();
+        }
+    }
+}
