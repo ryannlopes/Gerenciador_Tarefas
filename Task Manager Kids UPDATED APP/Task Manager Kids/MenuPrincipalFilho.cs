26d52b4d103cb26ce50951fa7b1c0babdbf93fe0
Panizo2 <88464143+Panizo2@users.noreply.github.com>
2023-06-06 13:54:32 -0300

    Registration and Login
    Both working

---
+++ Task Manager Kids UPDATED APP/Task Manager Kids/MenuPrincipalFilho.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_Manager_Kids
+{
+    public partial class MenuPrincipalFilho : Form
+    {
+        public MenuPrincipalFilho()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2GradientPanel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2Button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button10_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panelMain_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void guna2ProgressBar4_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button10_Click_1(object sender, EventArgs e)
+        {
+            TelaInicial Telainicial = new TelaInicial();
+            Telainicial.Show();
+            this.Hide();
+
+        }
+
+        private void guna2CirclePictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
