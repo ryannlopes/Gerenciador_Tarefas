cf72905a30edaa5e1b061791ce7d3bd55b307d49
Panizo2 <88464143+Panizo2@users.noreply.github.com>
2023-06-13 19:41:50 -0300

    Rewards Class

---
+++ TaskManagerKidsAPI/taskManagerKidsAPI/Models/Rewards.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace taskManagerKidsAPI.Models;
+
+public class Rewards
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string? Title { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string? Description { get; set; }
+    public int TaskId { get; set; }
+    public Task? Task { get; set; }
+}
