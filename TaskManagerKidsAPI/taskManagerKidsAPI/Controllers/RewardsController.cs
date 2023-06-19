1c18ec091453460f8e8b8db238346d5c69446804
Panizo2 <88464143+Panizo2@users.noreply.github.com>
2023-06-19 14:20:24 -0300

    Inserting the Reward Controller

---
+++ TaskManagerKidsAPI/taskManagerKidsAPI/Controllers/RewardsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using taskManagerKidsAPI.Context;
+using taskManagerKidsAPI.Models;
+using Reward = taskManagerKidsAPI.Models.Reward;
+
+namespace taskManagerKidsAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RewardsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RewardsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Reward>> Get()
+        {
+            var rewards = _context.Rewards.ToList();
+            if (rewards is null)
+            {
+                return NotFound("Resultados não encontrados...");
+            }
+            return rewards;
+        }
+    }
+}
--- TaskManagerKidsAPI/taskManagerKidsAPI/Models/Reward.cs
+++ TaskManagerKidsAPI/taskManagerKidsAPI/Models/Reward.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace taskManagerKidsAPI.Models;
 
-public class Rewards
+public class Reward
 {
     public int Id { get; set; }
     [Required]
